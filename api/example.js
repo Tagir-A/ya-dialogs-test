0a2b789a4c605a2f7e4c3de1e2a26587836f0f33
Tagir-A <tccup1@gmail.com>
2020-05-26 00:11:03 +0200

    1 file modified

--- api/example.js
+++ api/example.js
@@ -1,3 +1,10 @@
+// module.exports = (req, res) => {
+//   res.send('yolo')
+// }
 module.exports = (req, res) => {
-  res.send('yolo')
+  res.json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies
+  })
 }
