f3fc8eea343b0cad470fc27b960104640c6bf8a2
Tagir-A <tccup1@gmail.com>
2020-05-26 00:30:18 +0200

    1 file modified

--- api/alice.js
+++ api/alice.js
@@ -11,6 +11,8 @@ alice.any(ctx => Reply.text('Я вас не поняла'));
 module.exports = async (req, res) => {
   // Обработчики пойдут наверх искать подходящую команду
   // И составлять ответ на её основе.
+  console.log('req', req)
+  console.log('body', req.body)
   const aliceReply = await alice.handleRequest(req.body);
   res.send(aliceReply) 
 };
