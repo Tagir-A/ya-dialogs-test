3fbef4e1ab481b59cdfbadd793c312785976190f
Tagir-A <tccup1@gmail.com>
2020-05-26 00:23:39 +0200

    1 file added

---
+++ api/alice.js
@@ -0,0 +1,18 @@
+const { Alice, Reply } = require('yandex-dialogs-sdk');
+const alice = new Alice();
+
+alice.command('say my name', ctx => {
+  return Reply.text('mr white'); // you're goddamn right
+});
+
+alice.any(ctx => Reply.text('Я вас не поняла'));
+
+// Запуск асинхронного сервиса.
+module.exports = async (req, res) => {
+  // Из запроса извлекаются свойства request, session и version.
+  const request = await json(req);
+
+  // Обработчики пойдут наверх искать подходящую команду
+  // И составлять ответ на её основе.
+  return await alice.handleRequest(request);
+};
